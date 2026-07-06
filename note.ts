f83de5fdf5d72c9fb8396e2ec614159ee2d167d4
0xiloveyou <gamil.iloveyou@gmail.com>
2026-07-06 19:56:43 +0600

    Project initialization and folder setup

---
+++ note.ts
@@ -0,0 +1,136 @@
+/*
+
+
+> create project folder
+> terminal : -> git init
+> create file -> .gitignore 
+               ? add > 
+                      > node_modules
+                      > dist
+> terminal : 
+npm init
+npm install typescript tsx @types/node --save-dev
+npx tsc --init
+
+terminal answer : 
+package name : fixitnow
+0.0.1
+skip this part  description
+entry point : server.ts
+skip this part  test command
+skip this part git repository
+skip this part keywards
+skip this part keywards author
+skip this part keywards author license
+type : (commonjs) module
+
+terminal : 
+npm install prisma @types/pg --save-dev
+npm install @prisma/client @prisma/adapter-pg pg dotenv
+
+
+on tsconfig.json :
+select all thne pasete the code 
+{
+  "compilerOptions": {
+    "outDir": "./dist",
+    "module": "ESNext",
+    "moduleResolution": "bundler",
+    "target": "ES2023",
+    "types": ["node"],
+    "sourceMap": true,
+    "declaration": true,
+    "declarationMap": true,
+    "noUncheckedIndexedAccess": true,
+    "strict": true,
+    "isolatedModules": true,
+    "noUncheckedSideEffectImports": true,
+    "moduleDetection": "force",
+    "skipLibCheck": true
+  },
+
+  "exclude": ["node_modules", "dist"]
+}
+
+
+
+gitignore file add those: 
+generated
+.env
+
+termial : 
+npx prisma
+npx prisma init --output ../generated/prisma
+
+
+
+setup database url inside --->  .env
+.env.example file setup -> just example 
+
+
+Terminal :
+
+npm i express
+npm i @types/express --save-dev
+npm i bcryptjs
+npm i cors
+npm i @types/cors --save-dev
+npm i cookie-parser
+npm i @types/cookie-parser --save-dev
+npm i http-status
+npm i jsonwebtoken
+npm i @types/jsonwebtoken --save-dev
+
+
+in root folder : 
+crate folder : 
+> src
+   > create file 
+    > app.ts
+    > server.ts
+
+
+add this line on package.json
+  "scripts": {
+    "dev": "tsx watch src/server.ts",
+    "build" : "tsc",
+    "start"  : "node dist/server.js",
+
+terminal : 
+to build --> npm run build   
+to run   --> npm run dev
+
+
+
+create lib folder indde src
+then inded lib cerate file ->> prisma.ts
+then paset : 
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
+
+
+
+delete -> import { PrismaClient } from "../../generated/prisma/client";
+
+
+terminal :
+npx prisma migrate dev
+npx prisma generate
+
+
+import prisma client again insde the prisma.ts
+
+
+
+
+
+
+*/
---
+++ src/app.ts
@@ -0,0 +1,126 @@
+import express, { Application, NextFunction, request, Request, Response, Router } from "express";
+import cors from "cors"
+import config from "./config";
+// import { userRoutes } from "./modules/user/user.route";
+// import { authRoutes } from "./modules/auth/auth.routes";
+import cookieParser from "cookie-parser";
+// import { postRoutes } from "./modules/post/post.route";
+// import { commentRoutes } from "./modules/comment/comment.route";
+// import { notFound } from "./middlewares/notFound";
+// import httpStatus from "http-status"
+// import { globalErrorHandler } from "./middlewares/globalErrorHandler";
+// import { subscriptionRoutes } from "./modules/subscription/subscription.route";
+// import { stripe } from "./lib/stripe";
+// import { premiumRoutes } from "./modules/premium/premium.route";
+
+
+const app : Application = express()
+
+// app.use(cors({
+//    origin : config.app_url,
+//    credentials : true, // because of token and cokkie rretated stuff
+// }))
+
+
+// const endpointSecret = config.stripe_webhook_secret
+      
+/// keep this api endpoin before --> express.json() middlewere
+// app.post("/api/subscription/webhook", 
+//     express.raw({ type : 'application/json'}),
+//     (request, response) => {
+
+//   let event = request.body;
+//   console.log(event, "stripe request body")
+//   console.log(request.headers, "stripe req heareds")
+
+  
+//   if (endpointSecret) {
+    
+//     const signature = request.headers['stripe-signature']!;
+//     try {
+//       event = stripe.webhooks.constructEvent(
+//         request.body,
+//         signature,
+//         endpointSecret
+//       );
+//     } catch (err : any) {
+//       console.log(`⚠️  Webhook signature verification failed.`, err.message);
+//       return response.status(400).json({
+//          message : err.message
+//       })
+//     }
+//   }
+
+//   console.log(event, "event after try block")
+
+//   // Handle the event
+//   switch (event.type) {
+//     case 'payment_intent.succeeded':
+//       const paymentIntent = event.data.object;
+//       console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
+//       // Then define and call a method to handle the successful payment intent.
+//       // handlePaymentIntentSucceeded(paymentIntent);
+//       break;
+//     case 'payment_method.attached':
+//       const paymentMethod = event.data.object;
+//       // Then define and call a method to handle the successful attachment of a PaymentMethod.
+//       // handlePaymentMethodAttached(paymentMethod);
+//       break;
+//     default:
+//       // Unexpected event type
+//       console.log(`Unhandled event type ${event.type}.`);
+//   }
+
+//   // Return a 200 response to acknowledge receipt of the event
+//   response.send();
+//  }
+// )
+
+
+// app.use("/api/subscription/webhook", 
+//         express.raw({ type : 'application/json'}))
+/// here is no controller 
+// it match the route then goto last route then call the controller
+
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+app.use(cookieParser()) /// if we don't use it then we don't get cookiee
+                       /// we get undefined 
+                       /// so use that middlewer 
+                       /// else cn't get 0> req.cookies
+
+
+app.get("/", async (req : Request, res: Response) => {
+   // const user = await prisma.user.findMany()
+   // console.log(user) // test 
+
+   res.send("Hello, world!");
+ })
+
+// app.use("/api/users", userRoutes)
+// app.use("/api/auth", authRoutes)
+// app.use("/api/posts", postRoutes)
+// app.use("/api/comments", commentRoutes)
+// app.use("/api/subscription", subscriptionRoutes)
+// app.use("/api/premium", premiumRoutes)
+
+// /// sometimes we hit unknown url
+// /*
+// like : /api/ksladjf
+
+// in that case compile try to match the end point 
+
+// if no one match then at last 
+// it enters the 
+// last --> app.use()
+
+// then show the response
+// */
+// app.use(notFound)
+
+
+// /// all 4 paramiter is must 
+// // first err & last next 
+// app.use(globalErrorHandler)
+
+export default app;
