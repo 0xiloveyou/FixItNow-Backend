b070cbf311657f323842eb3ddadf4abd76b3bd9d
0xiloveyou <gamil.iloveyou@gmail.com>
2026-07-08 15:21:41 +0600

    Api end point data inserton file attached

---
+++ apiEndPointPostmanData.ts
@@ -0,0 +1,30 @@
+/*
+
+
+///--------------------------
+
+create category -> amidn access
+{
+  "name": "Plumbing",
+  "icon": "https://cdn-icons-png.flaticon.com/512/2966/2966481.png",
+  "description": "Services related to water pipes, leaks, faucets, and drainage systems."
+}
+
+{
+  "name": "Electrical",
+  "icon": "https://cdn-icons-png.flaticon.com/512/1048/1048953.png",
+  "description": "Electrical installation, repair, wiring, and maintenance services."
+}
+
+
+{
+  "name": "Cleaning",
+  "icon": "https://cdn-icons-png.flaticon.com/512/3082/3082037.png",
+  "description": "Home, office, kitchen, bathroom, and deep cleaning services."
+}
+
+///-------------------------------------
+
+
+
+*/
